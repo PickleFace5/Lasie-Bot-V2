df249b225ed956c495b32bfb5ee6c3340ef638a8
PickleFace5 <caden.dalley@icloud.com>
2022-01-01 10:41:00 -0800

    Added advanced URL Connection in /chad to specify what to download.

--- src/main/java/com/github/pickleface5/commands/imageing/ChadPfpCommand.java
+++ src/main/java/com/github/pickleface5/commands/imageing/ChadPfpCommand.java
@@ -1,7 +1,6 @@
 package com.github.pickleface5.commands.imageing;
 
 import com.github.pickleface5.util.EmbedUtils;
-import kong.unirest.Unirest;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
@@ -10,12 +9,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.FileImageOutputStream;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.StandardCopyOption;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Objects;
 
 public class ChadPfpCommand extends ListenerAdapter {
@@ -42,13 +44,52 @@ public class ChadPfpCommand extends ListenerAdapter {
             logger.trace(user);
         }
         event.deferReply().queue();
-        Graphics2D newImg = gigachad.createGraphics();
         try {
-            BufferedImage authorPfp = ImageIO.read(Unirest.get(user.getAvatarUrl()).asFile("src/main/resources/temp/" + user.getId() + ".png", StandardCopyOption.REPLACE_EXISTING).getBody());
-            newImg.drawImage(authorPfp, 170, 61, null);
+            URL url = new URL(Objects.requireNonNull(user.getAvatarUrl()));
+            URLConnection openConnection = url.openConnection();
+
+            try {
+                openConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+                openConnection.connect();
+            } catch (Exception e) {
+                event.getHook().sendMessage("There was an issue while requesting your profile photo.").queue();
+                e.printStackTrace();
+                return;
+            }
+            BufferedImage img = null;
+            try {
+                InputStream in = new BufferedInputStream(openConnection.getInputStream());
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                byte[] buf = new byte[1024];
+                int n;
+                while (-1 != (n = in.read(buf))) {
+                    out.write(buf, 0, n);
+                }
+                out.close();
+                in.close();
+                byte[] response = out.toByteArray();
+                img = ImageIO.read(new ByteArrayInputStream(response));
+            } catch (Exception e) {
+                System.out.println("I couldn't read an image from this link. The issue will be fixed soon.");
+                e.printStackTrace();
+            }
+            File authorPfp = new File("src/main/resources/temp/" + user.getId() + ".png");
+            try {
+                final ImageWriter writer = ImageIO.getImageWritersByFormatName("png").next();
+                writer.setOutput(new FileImageOutputStream(authorPfp));
+                assert img != null;
+                writer.write(null, new IIOImage(img, null, null), null);
+            } catch (IOException e) {
+                System.out.println("Couldn't create/send the output image.");
+                e.printStackTrace();
+            }
+            Graphics2D newImg = gigachad.createGraphics();
+            newImg.drawImage(ImageIO.read(authorPfp), 170, 61, null);
             ImageIO.write(gigachad, "png", new File("src/main/resources/temp/" + user.getId() + "_final.png"));
         } catch (IOException | NullPointerException e) {
-            event.getHook().sendMessage("There was an error while downloading your profile photo!").queue();
+            event.getHook().sendMessage("There was an error while downloading your profile photo! " +
+                    "Make sure you have a custom profile photo, not an discord profile photo. If it *still* doesn't " +
+                    "work for you, it's an issue on our end").queue();
             e.printStackTrace();
             return;
         }
