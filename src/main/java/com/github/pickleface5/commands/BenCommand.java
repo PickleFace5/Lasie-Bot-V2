707e070ffeed2ac3f190b71041e598c87143b29d
PickleFace5 <caden.dalley@icloud.com>
2022-05-12 16:56:20 -0700

    Added ben slash command.

---
+++ src/main/java/com/github/pickleface5/commands/BenCommand.java
@@ -0,0 +1,41 @@
+package com.github.pickleface5.commands;
+
+import com.github.pickleface5.util.EmbedUtils;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class BenCommand extends ListenerAdapter {
+    private static final Logger logger = LogManager.getLogger(BenCommand.class);
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        if (!event.getName().equals("ben")) return;
+        Random random = new Random(System.currentTimeMillis());
+        EmbedBuilder embedBuilder = new EmbedBuilder().setColor(EmbedUtils.EMBED_COLOR);
+        int type = random.nextInt(4);
+        if (type == 0) {
+            embedBuilder.setTitle("Yyyeeeessss...");
+            embedBuilder.setImage("https://c.tenor.com/6St4vNHkyrcAAAAd/yes.gif");
+            embedBuilder.setFooter("Ben says YES to: " + Objects.requireNonNull(event.getOption("question")).getAsString(), event.getUser().getAvatarUrl());
+        } else if (type == 1) {
+            embedBuilder.setTitle("No.");
+            embedBuilder.setImage("https://c.tenor.com/x2u_MyapWvcAAAAd/no.gif");
+            embedBuilder.setFooter("Ben says NO to: " + Objects.requireNonNull(event.getOption("question")).getAsString(), event.getUser().getAvatarUrl());
+        } else if (type == 2) {
+            embedBuilder.setTitle("Ho ho ho!");
+            embedBuilder.setImage("https://c.tenor.com/agrQMQjQTzgAAAAd/talking-ben-laugh.gif");
+            embedBuilder.setFooter("Ben LAUGHS at: " + Objects.requireNonNull(event.getOption("question")).getAsString(), event.getUser().getAvatarUrl());
+        } else {
+            embedBuilder.setTitle("Ughgh...");
+            embedBuilder.setImage("https://c.tenor.com/aomZLSiXCQ8AAAAC/ugh.gif");
+            embedBuilder.setFooter("Ben is DISGUSTED at: " + Objects.requireNonNull(event.getOption("question")).getAsString(), event.getUser().getAvatarUrl());
+        }
+        event.replyEmbeds(embedBuilder.build()).queue();
+    }
+}
