41e6709ecf85856ec389e2a503a2344504c617b4
PickleFace5 <caden.dalley@icloud.com>
2022-10-06 19:24:38 -0700

    Revert "Updated GAME_LIST"
    This reverts commit 472d27719afd430b5e4cbf95b56cefd0c471f73d.

--- src/main/java/com/github/pickleface5/user/BotStatus.java
+++ src/main/java/com/github/pickleface5/user/BotStatus.java
@@ -22,21 +22,12 @@ public class BotStatus {
             "Satisfactory", "Spore", "Stick Fight: The Game", "UNDERTALE", "Stardew Valley",
             "Ori and the Will of the Wisps", "RUST", "Ori and the Blind Forest", "Titanfall", "Tic Tac Toe",
             "Subnautica", "Subnautica: Below Zero", "Pikuniku", "Cyberpunk 2077", "osu!", "Universe Sandbox",
-            "PowerWash Simulator", "Muck", "Bloons TD 6", "Cookie Clicker", "Omori", "Don't Starve",
+            "PowerWash Simulator", "Muck", "Bloons Tower Defense 6", "Cookie Clicker", "Omori", "Don't Starve",
             "Don't Starve Together", "OneShot", "DELTARUNE", "Redmatch 2", "A Dance of Fire and Ice",
             "Sid Meier's Civilization VI", "Elden Ring", "ROUNDS", "LEGO® Star Wars™: The Skywalker Saga",
             "Forza Horizon 5", "Kerbal Space Program", "Kerbal Space Program 2", "It Takes Two", "EVE Online",
             "God of War", "Just Cause 3", "Halo Infinite", "Splitgate", "Tom Clancy's Rainbow Six® Siege",
-            "Gorilla Tag", "Geometry Dash", "Among Us VR", "Who's Your Daddy?!", "Portal", "Portal 2", "Overwatch 2",
-            "Kerbal Space Program", "Stray", "Enter the Gungeon", "Bloons TD 5", "ARK: Survival Evolved", "Bloons TD 4",
-            "Bloons TD 3", "Bloons TD 2", "Bloons Tower Defense", "Clash of Clans", "Clash Royale",
-            "Hatsune Miku: Project Diva X", "Risk of Rain", "Risk of Rain 2",
-            "Freddy Fazbear's Pizzeria Simulator", "Splatoon", "Splatoon 2", "Splatoon 3", "Mario Kart Wii",
-            "Mario Kart 8", "Wii Sports", "Wii Sports Resort", "Red Dead Redemption 2", "Mario Kart 8 Deluxe",
-            "Call of Duty: Black Ops II", "Call of Duty: Modern Warfare II", "Halo Reach", "Goat Simulator",
-            "Halo 3: ODST", "LittleBigPlanet", "LittleBigPlanet 2", "LittleBigPlanet 3", "Sackboy: A Big Adventure",
-            "ModNation Racers", "Mario + Rabbids Sparks of Hope", "The Legend of Zelda: Breath of the Wild",
-            "Super Smash Bros. Ultimate"};
+            "Gorilla Tag", "Geometry Dash", "Among Us VR", "Who's Your Daddy?!", "Portal", "Portal 2"};
 
     private static final ScheduledExecutorService scheduler =
             Executors.newScheduledThreadPool(1);
